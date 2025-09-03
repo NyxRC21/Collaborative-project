f8255d7cbc6a49fc06460636af616a968927afad
krisbelmadrigal-prog <krisbelmadrigal@gmail.com>
2025-09-02 23:05:23 -0600

    Agregue el archivo de Daniel con los cambios

--- Quiz4/Quiz4.java
+++ Quiz4/Quiz4.java
@@ -14,7 +14,8 @@ public class Quiz4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         System.out.println("¡Hola mundo desde GitHub colaborativo!");
+        System.out.println("Cambios de Kris en el programa de Daniel");
     }
 }
